85edab685f2ce6201724ccabb6ddb78ab9a6d708
Nadiia <wildfrenzy0432@gmail.com>
2023-04-15 22:34:30 +0200

    ex01 notdone

--- 00_module/ex01/Contact.hpp
+++ 00_module/ex01/Contact.hpp
@@ -5,9 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmaliare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/15 21:49:33 by nmaliare          #+#    #+#             */
-/*   Updated: 2023/04/15 21:49:35 by nmaliare         ###   ########.fr       */
+/*   Created: 2023/04/15 22:21:02 by nmaliare          #+#    #+#             */
+/*   Updated: 2023/04/15 22:21:02 by nmaliare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#ifndef CODE_CONTACT_HPP
+#define CODE_CONTACT_HPP
+
+
+class Contact {
+	char *contact;
+};
+
+
+#endif //CODE_CONTACT_HPP
--- 00_module/ex01/PhoneBook.hpp
+++ 00_module/ex01/PhoneBook.hpp
@@ -5,9 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmaliare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/15 21:48:32 by nmaliare          #+#    #+#             */
-/*   Updated: 2023/04/15 21:48:37 by nmaliare         ###   ########.fr       */
+/*   Created: 2023/04/15 22:19:56 by nmaliare          #+#    #+#             */
+/*   Updated: 2023/04/15 22:19:56 by nmaliare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CODE_PHONEBOOK_HPP
+#define CODE_PHONEBOOK_HPP
 
+class PhoneBook {
+	char contacts[7][40];
+};
+
+#endif
