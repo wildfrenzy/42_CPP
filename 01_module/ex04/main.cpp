cfdbd173ef5cb4a458b5602ea8d2fb55e874019d
Nadiia <wildfrenzy0432@gmail.com>
2023-04-21 18:09:31 +0200

    04 done

--- 01_module/ex04/main.cpp
+++ 01_module/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nmaliare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 16:13:19 by nmaliare          #+#    #+#             */
-/*   Updated: 2023/04/20 16:58:08 by nmaliare         ###   ########.fr       */
+/*   Updated: 2023/04/21 16:58:00 by nmaliare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,31 +33,24 @@ int main(int ac, char *av[])
 		std::cout << "Failed to open " << av[1] << std::endl;
 		return -1;
 	}
-	std::string		buff1;
 	std::string		buff2;
+	std::string		result;
 
 	ifs >> buff2;
-	while (ifs >> buff1)
-		buff2 += " " + buff1;
-	
-
-	std::cout << "s1: " << s1 << std::endl;
-	std::cout << "s2: " << s2 << std::endl;
-	std::cout << "replaceFileName: " << replaceFileName << std::endl;
-	std::cout << "buff2 : " << buff2 << std::endl;
-	std::cout << "buff1: " << buff1 << std::endl;
+	while (ifs >> result)
+		buff2 += " " + result;
 	ifs.close();
-
-
-/*	std::ifstream	ifs("numbers"); //Input file stream
-	unsigned int	dst;
-	unsigned int	dst2;
-
-	ifs >> dst >> dst2;
-	std::cout << dst << " . " << dst2 <<std::endl;
-	ifs.close();
-
-	std::ofstream ofs("test.out"); //Output file stream
-	ofs << " save me" << std::endl;*/
+	result.erase(0,std::string::npos);
+	std::size_t found = buff2.find(s1);
+	while (found != std::string::npos)
+	{
+		result += buff2.substr(0, found) + s2;
+		if (found != 0) buff2.erase(0, found + s1.length());
+		else buff2.erase(found, s1.length());
+		found = buff2.find(s1);
+	}
+	if (!buff2.empty()) result += buff2;
+	std::ofstream ofs(replaceFileName.c_str());
+	ofs << result << std::endl;
 	return 0;
 };
