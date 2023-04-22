7452f0c3a6b556db1228c2443157665e141ea3c4
Nadiia <wildfrenzy0432@gmail.com>
2023-04-22 19:07:34 +0200

    module 02 started. 00 done, i think

---
+++ 02_module/ex00/Fixed.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaliare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/22 16:29:41 by nmaliare          #+#    #+#             */
+/*   Updated: 2023/04/22 19:06:54 by nmaliare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _val(0){
+	std::cout<< "Default constructor called" << std::endl;
+}
+Fixed::~Fixed() {
+	std::cout<< "Destructor called" << std::endl;
+}
+Fixed::Fixed(const Fixed &num) {
+	std::cout<< "Copy constructor called" << std::endl;
+	this->_val = num._val; // ?
+}
+
+Fixed &Fixed::operator=(const Fixed &) {
+	std::cout<< "Copy assignment operator called" << std::endl;
+	return *this;
+}
+
+int Fixed::getRawBits() const {
+	std::cout<< "getRawBits member function called" << std::endl;
+	return this->_val;
+}
+
+void Fixed::setRawBits(const int raw) {
+	this->_val = raw;
+}
+
+const int Fixed::_bits = 8;
---
+++ 02_module/ex00/Fixed.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmaliare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/22 16:29:41 by nmaliare          #+#    #+#             */
+/*   Updated: 2023/04/22 16:29:41 by nmaliare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &num); // copy constructor
+	Fixed& operator=(const Fixed &);
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+
+private:
+	int					_val;
+	static const int	_bits;
+};
+
+
+#endif
