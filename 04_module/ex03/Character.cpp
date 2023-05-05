694550eced40825c62415da274b997c17f490667
Nadiia <wildfrenzy0432@gmail.com>
2023-05-05 20:29:10 +0200

    04_module ex03 Character.cpp

--- 04_module/ex03/Character.cpp
+++ 04_module/ex03/Character.cpp
@@ -14,10 +14,16 @@
 
 Character::Character() {
 	std::cout << "Character default constructor called" << std::endl;
+	for (int i = 0; i < 4; i++) {
+		this->_inventory[i] = NULL;
+	}
 }
 
 Character::Character(std::string name) : _name(name) {
 	std::cout << "Character constructor for "<< name << " called" << std::endl;
+	for (int i = 0; i < 4; i++) {
+		this->_inventory[i] = NULL;
+	}
 }
 
 Character::Character(const Character &ch) {
@@ -31,12 +37,21 @@ Character &Character::operator=(const Character &ch) {
 	if (this == &ch)
 		return *this;
 	this->_name = ch._name;
-//	this->inventory = ch.clone(); // clone all 4, not 1
+	for (int i = 0; i < 4; i++) {
+		delete this->_inventory[i];
+	}
+	for (int i = 0; i < 4; i++) {
+		if (ch._inventory[i])
+			this->_inventory[i] = ch._inventory[i]->clone();
+	}
 	return *this;
 }
 
 Character::~Character(){
 	std::cout << "Character destructor called" << std::endl;
+	for (int i = 0; i < 4; i++) {
+		delete this->_inventory[i];
+	}
 }
 
 const std::string &Character::getName() const {return this->_name;}
